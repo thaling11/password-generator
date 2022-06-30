6ac7ce0b39d62736497935d18e393333a42c4ee3
thaling <thaling@verizon.net>
2022-06-30 17:44:58 -0600

    README.md start

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,5 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
 var characterLength = 8;
 var characterArr = [];
 var specialCharacterArr = ["!", "@", "#", "%", "&", "*", "^", "+", "?", "<", ">"];
@@ -10,22 +7,62 @@ var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numberArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 
+// Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  getPrompts();
+    var correctPrompts = getPrompts();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    if(correctPrompts) {
+        var randomPassword = generatePassword();
 
+        passwordText.value = randomPassword;
+    } 
+  else {
+    passwordText.value = "";
+  }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 function generatePassword() {
-
-
+    var password = "";
+    for(var i = 0; i < characterLength; i++) {
+        var randomLetter = Math.floor(Math.random() * characterArr.length);
+        password = password + characterArr[randomLetter];
+    }
+    return password;
 }
 
+
 function getPrompts(){
+  characterArr = [];
+
   characterLength = parseInt(prompt("How many characters do you want your password to be? 8 to 128 chaarcters"))
+
+
+if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+  alert("Must be between 8 and 128 characters.");
+  return false;
+}
+
+if (confirm("Would you like lowercase letters?")) {
+  characterArr = characterArr.concat(lowerCaseArr)
+}
+
+if (confirm("Would you like uppercase letters?")) {
+  characterArr = characterArr.concat(upperCaseArr);
+}
+
+if (confirm("Would you like special characters?")) {
+  characterArr = characterArr.concat(specialCharacterArr);
+}
+
+if (confirm("Would you like numbers?")) {
+  characterArr = characterArr.concat(numberArr)
+}
+return true;
+
 }
